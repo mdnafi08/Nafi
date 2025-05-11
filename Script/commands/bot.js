8068593c81920b322bc65a348cf1850851528a75
mnafi1 <mnafi764@gmail.com>
2025-05-12 02:42:14 +0600

    Update bot.js

--- Script/commands/bot.js
+++ Script/commands/bot.js
@@ -91,11 +91,11 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("সরি বস মাফ করে দেন আর এমন ভুল হবে না🥺🙏", threadID);
    };
 
-   if ((event.body.toLowerCase() == "@Sinthiya Jannat") || (event.body.toLowerCase() == "@Farhana Ontora ")) {
+   if ((event.body.toLowerCase() == "@Sinthiya Jannat") || (event.body.toLowerCase() == "@Sinthiya Jannat")) {
      return api.sendMessage("খবরদার কেউ এই আইড়ি মেনশন দিবানা এটা আমার বস Nafi এর বউ এর আইড়ি😠🥰⛏️", threadID);
    };
 
-  if ((event.body.toLowerCase() == "Sinthiya") || (event.body.toLowerCase() == "arohi")) {
+  if ((event.body.toLowerCase() == "Sinthiya") || (event.body.toLowerCase() == "sinthiya")) {
      return api.sendMessage("খবরদার কেউ এই নাম দরে ডাক দিবানা এটা আমার বস Nafi এর বউ এর নাম..!😠🥰⛏️", threadID);
    };
   
